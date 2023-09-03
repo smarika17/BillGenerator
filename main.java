4cb04e6de27cc6115e922ff52933bf8d1cf2c85a
smarika17 <114747817+smarika17@users.noreply.github.com>
2023-09-03 18:13:14 +0530

    Add files via upload
    Parent class and main class

---
+++ main.java
@@ -0,0 +1,14 @@
+
+public class main {
+    public static void main(String[] args) {
+        // Pizza basePizza=new Pizza(true);
+        // basePizza.addExtraCheese();
+        // basePizza.addExtraToppings();
+        // basePizza.takeAway();
+        // basePizza.getBill();
+
+        DeluxePizza dp= new DeluxePizza(true);
+        dp.getBill();
+        dp.takeAway();
+    }
+}
